4a4949a3bd0dd599f77349d64896af3f77b598cd
Akihiro <43564475+awakuwaku@users.noreply.github.com>
2020-10-07 23:02:19 +0900

    Refactor

--- src/test/kotlin/com/example/controllers/GreetingControllerTest.kt
+++ src/test/kotlin/com/example/controllers/GreetingControllerTest.kt
@@ -3,6 +3,7 @@ package com.example.controllers
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
 import org.springframework.restdocs.JUnitRestDocumentation
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation
@@ -16,7 +17,9 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder
 
 class GreetingControllerTest {
-  private var mockMvc: MockMvc? = null
+
+  @Autowired
+  private lateinit var mockMvc: MockMvc
 
   @Rule @JvmField
   val restDocumentation = JUnitRestDocumentation("build/generated-snippets")
@@ -39,7 +42,7 @@ class GreetingControllerTest {
   @Test
   @Throws(Exception::class)
   fun GoodMorning下限_正常系() {
-    mockMvc!!.perform(MockMvcRequestBuilders.get("/greeting").param("hour", "5"))
+    mockMvc.perform(MockMvcRequestBuilders.get("/greeting").param("hour", "5"))
       .andExpect(MockMvcResultMatchers.status().isOk)
       .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
       .andExpect(MockMvcResultMatchers.jsonPath("$.greeting").value("good morning"))
@@ -48,7 +51,7 @@ class GreetingControllerTest {
   @Test
   @Throws(Exception::class)
   fun GoodMorning上限_正常系() {
-    mockMvc!!.perform(MockMvcRequestBuilders.get("/greeting").param("hour", "15"))
+    mockMvc.perform(MockMvcRequestBuilders.get("/greeting").param("hour", "15"))
       .andExpect(MockMvcResultMatchers.status().isOk)
       .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
       .andExpect(MockMvcResultMatchers.jsonPath("$.greeting").value("good morning"))
@@ -57,7 +60,7 @@ class GreetingControllerTest {
   @Test
   @Throws(Exception::class)
   fun GoodNight下限1_正常系() {
-    mockMvc!!.perform(MockMvcRequestBuilders.get("/greeting").param("hour", "0"))
+    mockMvc.perform(MockMvcRequestBuilders.get("/greeting").param("hour", "0"))
       .andExpect(MockMvcResultMatchers.status().isOk)
       .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
       .andExpect(MockMvcResultMatchers.jsonPath("$.greeting").value("good night"))
@@ -66,7 +69,7 @@ class GreetingControllerTest {
   @Test
   @Throws(Exception::class)
   fun GoodNight下限2_正常系() {
-    mockMvc!!.perform(MockMvcRequestBuilders.get("/greeting").param("hour", "16"))
+    mockMvc.perform(MockMvcRequestBuilders.get("/greeting").param("hour", "16"))
       .andExpect(MockMvcResultMatchers.status().isOk)
       .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
       .andExpect(MockMvcResultMatchers.jsonPath("$.greeting").value("good night"))
@@ -75,7 +78,7 @@ class GreetingControllerTest {
   @Test
   @Throws(Exception::class)
   fun GoodNight上限1_正常系() {
-    mockMvc!!.perform(MockMvcRequestBuilders.get("/greeting").param("hour", "4"))
+    mockMvc.perform(MockMvcRequestBuilders.get("/greeting").param("hour", "4"))
       .andExpect(MockMvcResultMatchers.status().isOk)
       .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
       .andExpect(MockMvcResultMatchers.jsonPath("$.greeting").value("good night"))
@@ -84,7 +87,7 @@ class GreetingControllerTest {
   @Test
   @Throws(Exception::class)
   fun doc() {
-    mockMvc!!.perform(MockMvcRequestBuilders.get("/greeting").param("hour", "24"))
+    mockMvc.perform(MockMvcRequestBuilders.get("/greeting").param("hour", "24"))
       .andExpect(MockMvcResultMatchers.status().isOk)
       .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
       .andExpect(MockMvcResultMatchers.jsonPath("$.greeting").value("good night"))
@@ -102,7 +105,7 @@ class GreetingControllerTest {
   @Test
   @Throws(Exception::class)
   fun 入力値範囲外_下限越え() {
-    mockMvc!!.perform(MockMvcRequestBuilders.get("/greeting").param("hour", "-1"))
+    mockMvc.perform(MockMvcRequestBuilders.get("/greeting").param("hour", "-1"))
       .andExpect(MockMvcResultMatchers.status().isBadRequest)
       .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
   }
@@ -110,7 +113,7 @@ class GreetingControllerTest {
   @Test
   @Throws(Exception::class)
   fun 入力値範囲外_上限超え() {
-    mockMvc!!.perform(MockMvcRequestBuilders.get("/greeting").param("hour", "25"))
+    mockMvc.perform(MockMvcRequestBuilders.get("/greeting").param("hour", "25"))
       .andExpect(MockMvcResultMatchers.status().isBadRequest)
       .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
   }
@@ -118,7 +121,7 @@ class GreetingControllerTest {
   @Test
   @Throws(Exception::class)
   fun 入力値範囲外_数値外入力() {
-    mockMvc!!.perform(MockMvcRequestBuilders.get("/greeting").param("hour", "123abc"))
+    mockMvc.perform(MockMvcRequestBuilders.get("/greeting").param("hour", "123abc"))
       .andExpect(MockMvcResultMatchers.status().isBadRequest)
       .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
   }
